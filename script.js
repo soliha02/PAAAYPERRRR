dad3b21552126cee60e9d786e72bd9dbfc6efb6b
soliha02 <164037162+soliha02@users.noreply.github.com>
2024-06-02 14:18:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+
+
+if ('Saytimga hush kelibsiz <3') {
+    
+} else {
+    'Siz haqiqiy dasturchi emassiz 😭🙁'
+}
+
+
+
+let ismm = prompt("Random chiqqan son : 53 🧨");
+let familiyaa = prompt("53 soni fizz ham buzz ham fizzbazz ham emas");
+alert(ismm,familiyaa)
+
+
+let q = prompt("Random chiqqan son : 63 🎋");
+let qq = prompt("63:fizz");
+alert(q,qq)
+
+
+let w = prompt("Random chiqqan son : 99 🎏");
+let ww = prompt("99:fizz");
+alert(w,ww)
+
+
+
+let ismmmmm = prompt("Random chiqqan son : 52 🎑");
+let familiyaaaaa = prompt("52 soni fizz ham buzz ham fizzbazz ham emas");
+alert(ismmmmm,familiyaaaaa)
+
+
+
+let ismmmmmm = prompt("Random chiqqan son : 46 🎍");
+let familiyaaaaaa = prompt("46 soni fizz ham buzz ham fizzbazz ham emas");
+alert(ismmmmmm,familiyaaaaaa)
+
+
+
+let ismmmmmmm = prompt("Random chiqqan son : 8 🎃");
+let familiyaaaaaaa = prompt("8 soni fizz ham buzz ham fizzbazz ham emas");
+alert(ismmmmmmm,familiyaaaaaaa)
+
+
+
+let ismmmmmmmm = prompt("Random chiqqan son : 67 🎫");
+let familiyaaaaaaaa = prompt("49 soni fizz ham buzz ham fizzbazz ham emas");
+alert(ismmmmmmmm,familiyaaaaaaaa)
+
+
+let ha = prompt("Random chiqqan son : 49 🎫");
+let sur = prompt("49 soni fizz ham buzz ham fizzbazz ham emas");
+alert(ha,sur)
+
+let ismmmmmmmmm = prompt("Random chiqqan son : 20 🎫");
+let familiyaaaaaaaaa = prompt("49 soni fizz ham buzz ham fizzbazz ham emas");
+alert(ismmmmmmmmm,familiyaaaaaaaaa)
+
+
+
+
